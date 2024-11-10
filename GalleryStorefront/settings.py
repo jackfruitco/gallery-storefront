4d644afab01b6448d21512b5753556e386e0e900
Tyler J <tyler@jackfruit.me>
2024-11-09 19:12:11 -0500

    adds Postgres support for production environment

--- GalleryStorefront/settings.py
+++ GalleryStorefront/settings.py
@@ -87,17 +87,27 @@ WSGI_APPLICATION = 'GalleryStorefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("POSTGRES_DB"),
+            'USER': os.getenv("POSTGRES_USER"),
+            'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+            'HOST': os.getenv("POSTGRES_HOST"),
+            'PORT': os.getenv("POSTGRES_PORT")
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- apps/gallery/admin.py
+++ apps/gallery/admin.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib import admin
 from .models import Product, ProductImage, Color, ProductCategory
 from apps.shopify_app.decorators import shopify_token_required
@@ -8,6 +9,7 @@ class MediaUploadInline(admin.StackedInline):
     extra = 1
     max_num = 4
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ("shop_global_id", "created_at", "modified_at")
 
@@ -22,6 +24,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ["category", "display", "shop_sync"]
     search_fields = ["name", "description", "shop_global_id"]
 
-admin.site.register(Product, ProductAdmin)
+    def save_model(self, request, obj, form, change):
+        super(ProductAdmin, self).save_model(request, obj, form, change)
+        if obj.shop_global_id:
+            msg = 'The product "%s" was synced successfully with Shopify in %s status.' % (obj.name, obj.shop_status)
+            messages.add_message(request, messages.INFO, msg)
+
+# admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductCategory)
 admin.site.register(Color)
