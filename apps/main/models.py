f499ba0b9d7b49705c854792e1622f8edbe1689f
Tyler J <tyler@jackfruit.me>
2025-01-02 09:28:40 -0500

    improves S3 functionality

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,50 +1,26 @@
 from django.db import models
-from django.conf import settings
 from autoslug import AutoSlugField
 
-# class Profile(models.Model):
-#    first_name = models.CharField(max_length=50)
-#    middle_name = models.CharField(max_length=50, blank=True)
-#    last_name = models.CharField(max_length=50)
-#
-#    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, blank=True, null=True)
-#
-#    position = models.CharField(max_length=50, blank=True)
-#
-#    quote = models.TextField(blank=True)
-#    about_me = models.TextField(blank=True)
-#    portrait = models.ImageField(upload_to='media/images/profiles/', blank=True)
-#
-#    display_on_home = models.BooleanField(default=True)
-#    active = models.BooleanField(default=True)
-#
-#    data_verified = models.DateTimeField(auto_now=True)
-#
-#    def full_name(self):
-#        if self.middle_name: return f"{self.first_name} {self.middle_name} {self.last_name}"
-#        else: return f"{self.first_name} {self.last_name}"
-#
-#    def first_mi_last(self):
-#        mi = self.middle_name[:1]
-#        if self.middle_name: return f"{ self.first_name } { mi } { self.last_name }"
-#        else: return f"{self.first_name} {self.last_name}"
-#
-#    slug = AutoSlugField(populate_from='first_mi_last', unique=True, always_update=True, editable=True)
-#
-#    def __str__(self):
-#        return self.full_name()
-#
-# class Contact(models.Model):
-#    contact_name = models.ForeignKey(Profile, on_delete=models.CASCADE)
-#    contact_type = {
-#        "email": "Email",
-#        "phone": "Phone",
-#        "website": "Website",
-#        "instagram": "Instagram",
-#    }
-#    contact_data = models.TextField()
-#    contact_created_at = models.DateTimeField(auto_now_add=True)
-#    contact_modified_at = models.DateTimeField(auto_now=True)
+def _get_image_path(instance, filename):
+    """Returns constructed file path for media"""
+    return "images/SiteMedia/{0}".format(
+        instance.slug + "." + filename.split('.')[-1]
+    )
 
-#    def __str__(self):
-#        return f"{self.contact_name}_{self.contact_data}"
+class SiteMedia(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    description = models.CharField(
+        max_length=100,
+        blank=False,
+        help_text="3-5 words describing the image"
+    )
+    slug = AutoSlugField(
+        populate_from='description',
+        unique_with='fk_product',
+        always_update=True
+    )
+    image = models.ImageField(
+        upload_to=_get_image_path
+    )
