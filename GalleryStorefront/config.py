4d46b4fe9fa1fa5b4c5c3410750cc426a25f03a1
Tyler J <tyler@jackfruit.me>
2025-01-06 13:56:50 -0500

    adds S3 Config Dataclass

--- GalleryStorefront/config.py
+++ GalleryStorefront/config.py
@@ -2,6 +2,7 @@
 # Can be imported to any python module for use
 
 import os
+from dataclasses import dataclass
 
 STOREFRONT_URL = os.getenv('STOREFRONT_URL', '')
 
@@ -18,3 +19,33 @@ def configure_s3(**kwargs):
     for key, value in kwargs.items():
         config[key] = value
     return config
+
+@dataclass
+class S3_Config:
+    bucket_name: str = os.getenv('CLOUDFLARE_R2_BUCKET', None)
+    default_acl: str = os.getenv('CLOUDFLARE_R2_ACL', 'public-read')
+    signature_version: str = os.getenv('CLOUDFLARE_R2_SIGNATURE', 's3v4')
+    endpoint_url: str = os.getenv('CLOUDFLARE_R2_ENDPOINT', None)
+    access_key: str = os.getenv('CLOUDFLARE_R2_ACCESS_KEY', None)
+    secret_key: str = os.getenv('CLOUDFLARE_R2_SECRET_KEY', None)
+
+    def __json__(self, **kwargs) -> dict:
+        """
+        Return JSON-serializable dict with configuration values for S3.
+
+        :param kwargs: For additional S3 configuration options
+        :return: JSON serializable dict
+        """
+
+        config = {
+            self.bucket_name.__name__: self.bucket_name,
+            self.default_acl.__name__: self.default_acl,
+            self.signature_version.__name__: self.signature_version,
+            self.endpoint_url.__name__: self.endpoint_url,
+            self.access_key.__name__: self.access_key,
+            self.secret_key.__name__: self.secret_key,
+        }
+
+        for key, value in kwargs.items():
+            config[key] = value
+        return config
