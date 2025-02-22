eeadfa019cf3c6a8b1770467b59abee4c4458a6c
Tyler J <tyler@jackfruit.me>
2025-02-22 17:28:54 -0500

    Adds site description context processor.

--- apps/main/context_processors.py
+++ apps/main/context_processors.py
@@ -2,4 +2,7 @@ import os
 
 
 def main(request):
-    return {"SITE_NAME": os.getenv("SITE_NAME", "Gallery Storefront")}
+    return {
+        "SITE_NAME": os.getenv("SITE_NAME", "Gallery Storefront"),
+        "SITE_DESCRIPTION": os.getenv("SITE_DESCRIPTION", ""),
+    }
