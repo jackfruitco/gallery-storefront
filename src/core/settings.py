bee17dead1d5d910eea5456be0f6c58383a40030
Tyler J <tyler@jackfruit.me>
2025-03-23 19:08:49 -0400

    change STATIC_ROOT to project root instead of Django root
    The static_root should be in /app, not /app/src

--- src/core/settings.py
+++ src/core/settings.py
@@ -160,14 +160,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR.parent / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
 # Media files (uploaded by users)
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR.parent / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
